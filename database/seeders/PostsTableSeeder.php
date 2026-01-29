660702b6d4008e39f5bdea157d8c8fca5b2be4ad
Arnau Canet <arnaucanetfp@ibf.cat>
2026-01-29 15:48:10 +0100

    Implement CRUD functionality for posts with PostController and StorePostRequest, update .env and .gitignore, and adjust package.json

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('posts')->delete();
+        
+        
+        
+        
+    }
+}
