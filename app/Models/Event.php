fb9beba8356173d6a9e0f9b1fea09917cea535a9
Arnau Canet <arnaucanetfp@ibf.cat>
2026-02-18 17:26:42 +0100

    feat: add user relationship to Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,6 +11,9 @@ class Event extends Model
 
     protected $fillable = ['title', 'description', 'start_date', 'end_date', 'capacity', 'featured', 'status', 'user_id'];
 
-
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
