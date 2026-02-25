bce3107a5b02a7a29c17618d44f797709fe71d05
Arnau Canet <arnaucanetfp@ibf.cat>
2026-02-25 19:34:48 +0100

    feat: enhance date formatting to include time in categories

--- resources/js/views/admin/categories/Index.vue
+++ resources/js/views/admin/categories/Index.vue
@@ -158,7 +158,9 @@ const formatDate = (dateStr) => {
   return date.toLocaleDateString('es-ES', {
     year: 'numeric',
     month: 'short',
-    day: 'numeric'
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
   });
 };
 
